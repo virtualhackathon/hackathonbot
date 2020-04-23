dbb78b768160316b927c8ff61635eb865c2a4770
Mark Tyneway <mark.tyneway@gmail.com>
2020-04-23 16:41:02 -0700

    test: database

--- test/database-test.js
+++ test/database-test.js
@@ -1,5 +1,5 @@
 /**
- *
+ * HackathonDB tests
  */
 
 const HackathonDB = require('../lib/hackathondb.js');
@@ -16,7 +16,7 @@ describe('HackathonDB', function() {
   this.timeout(10000);
 
   before(async () => {
-    await logger.open();
+    //await logger.open();
 
     db = new HackathonDB({
       memory: true, // TODO: memory isn't being applied
@@ -24,7 +24,6 @@ describe('HackathonDB', function() {
       logger: logger
     });
 
-    await db.open();
   });
 
   after(async () => {
@@ -33,11 +32,12 @@ describe('HackathonDB', function() {
   });
 
   beforeEach(async () => {
-    //
+    await db.open();
   });
 
   afterEach(async () => {
-    // clear the db
+    await db.deleteDB();
+    await db.close();
   });
 
   it('should create/get event', async () => {
@@ -46,15 +46,17 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://bitcoin.com',
-      message: 'this is a test!'
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      message: 'this is a test!',
+      open: false,
+      btcFee: null,
+      hnsFee: null
     };
 
     await db.createEvent(event);
 
     const result = await db.getEventByName('foobar');
 
-    // The id is an autoincrementing primary key
-    delete result.id;
     assert.deepEqual(event, result);
   });
 
@@ -70,7 +72,6 @@ describe('HackathonDB', function() {
 
     const result = await db.getUser('satoshi', 'irc.freenode.net');
 
-    delete result.id;
     assert.deepEqual(user, result);
   });
 
@@ -86,7 +87,6 @@ describe('HackathonDB', function() {
     await db.createAddress(address);
 
     const result = await db.getAddressByUserIdAndEventId(0, 0);
-    delete result.id;
 
     assert.deepEqual(address, result);
   });
@@ -108,10 +108,18 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://dns.live',
-      message: 'this is a not a drill!'
+      message: 'this is a not a drill!',
+      open: false,
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
     };
 
     await db.createEvent(event);
+
+    await db.addUserToEventByNickAndServer({
+      nick: user.nick,
+      server: user.server,
+      event: event.name
+    });
 
     const address = {
       nick: 'testnick',
@@ -131,13 +139,17 @@ describe('HackathonDB', function() {
     assert.equal(address.address, result.address);
 
     {
-      const userResult = await db.getUser(user.nick, user.server);
-      assert(userResult);
+      const info = await db.getUser(user.nick, user.server);
+      assert(info);
     }
 
     {
-      const eventResult = await db.getEventByName(event.name);
-      assert.equal(result.eventId, eventResult.id);
+      // get users by event
+      const info = await db.getUserAddressesByEvent(event.name);
+      assert.equal(info.length, 1);
+
+      assert.equal(info[0].user.nick, user.nick);
+      assert.equal(info[0].address.address, address.address);
     }
   });
 
@@ -151,6 +163,7 @@ describe('HackathonDB', function() {
       isSponsor: false,
       link: 'twitter.com/bitcoin'
     };
+
     await db.createUser(user);
 
     // Create event
@@ -159,15 +172,17 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://dns.live',
-      message: 'this is a message'
+      message: 'this is a message',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
     };
 
     await db.createEvent(event);
 
     const addresses = [
-      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}`,
-      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}`,
-      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}`
+      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${randomStr(3)}`,
+      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${randomStr(3)}`,
+      `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${randomStr(3)}`
     ];
 
     // add addresses
@@ -207,7 +222,9 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://dns.live',
-      message: 'this is a message'
+      message: 'this is a message',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
     };
 
     await db.createEvent(event);
@@ -219,12 +236,8 @@ describe('HackathonDB', function() {
     });
 
     const result = await db.getUsersByEvent(event.name);
-
-    {
-      const eventByName = await db.getEventByName(event.name);
-      const byEventId = await db.getUsersByEvent(eventByName.id);
-      assert.deepEqual(result, byEventId);
-    }
+    assert.equal(result.length, 1);
+    assert.equal(result[0].nick, user.nick);
   });
 
   it('should get many users for event', async () => {
@@ -234,7 +247,9 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://sia.tech',
-      message: 'my message'
+      message: 'my message',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
     };
 
     await db.createEvent(event);
@@ -275,7 +290,9 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://leader.xi',
-      message: 'do you know?'
+      message: 'do you know?',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
     };
 
     await db.createEvent(event);
@@ -325,6 +342,7 @@ describe('HackathonDB', function() {
       start: 1586372294397,
       end: 1586372196415,
       link: 'https://leader.xi',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
       message: 'do you know?'
     };
 
@@ -368,74 +386,245 @@ describe('HackathonDB', function() {
     assert.deepEqual(users[0], user);
   });
 
-  it('should get all events', async () => {
-    this.skip();
-
-    const events = await db.getEvents();
-
-    assert(events.length > 0);
-  });
-
-  it('should get address user pairs for event', async () => {
-    this.skip();
-
-    const str = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-
-    // Create event
+  it('should update event', async () => {
     const event = {
-      name: `address-user-pairs-${str}`,
+      name: 'update-event',
       start: 1586372294397,
       end: 1586372196415,
-      link: 'https://winner.ogh',
-      message: 'open your eyes'
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
     };
 
     await db.createEvent(event);
 
-    // create 2 users
-    // loop over the users
+    const pre = await db.getEventByName(event.name);
+    assert.equal(pre.name, event.name);
+
+    const update = {
+      name: event.name,
+      open: true,
+      btcFee: 100000,
+      hnsFee: 1000,
+      link: 'https://leader.xy',
+      message: 'do you know?'
+    };
+
+    await db.updateEvent(update);
+
+    const post = await db.getEventByName(event.name);
+    assert.equal(post.name, event.name);
+    assert.equal(post.open, update.open);
+    assert.equal(post.btcFee, update.btcFee);
+    assert.equal(post.hnsFee, update.hnsFee);
+    assert.equal(post.link, update.link);
+    assert.equal(post.message, update.message);
+  });
+
+  it('should create tournament', async () => {
+    const event = {
+      name: 'create-tournament',
+      start: 1586372294397,
+      end: 1586372196415,
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
+    };
+
+    await db.createEvent(event);
+
+    const tournament = {
+      name: 'big-pr',
+      event: event.name,
+      link: 'https://github.com/bcoin-org/bcoin/issues/555',
+      message: 'Do this for the win',
+      percentage: '0.25'
+    };
+
+    await db.createTournament(tournament);
+
+    const tournaments = await db.getTournamentsByEvent(event.name);
+    assert.equal(tournaments.length, 1);
+    assert.equal(tournaments[0].name, tournament.name);
+    assert.equal(tournaments[0].link, tournament.link);
+    assert.equal(tournaments[0].message, tournament.message);
+    assert.equal(tournaments[0].percentage, tournament.percentage);
+
+    const t = await db.getTournamentByNameAndEvent(tournament.name, event.name);
+    assert.equal(tournament.name, t.name);
+    assert.equal(tournament.link, t.link);
+    assert.equal(tournament.message, t.message);
+    assert.equal(tournament.percentage, t.percentage);
+  });
+
+  it('should update tournament', async () => {
+    const event = {
+      name: 'update-tournament',
+      start: 1586372294397,
+      end: 1586372196415,
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
+    };
+
+    await db.createEvent(event);
+
+    const tournament = {
+      name: 'update-test',
+      event: event.name,
+      link: 'https://github.com/bcoin-org/bcoin/issues/555',
+      message: 'Do this for the win',
+      percentage: '0.25'
+    };
+
+    await db.createTournament(tournament);
+
+    const pre = await db.getTournamentByNameAndEvent(tournament.name, event.name);
+
+    const update = {
+      name: tournament.name,
+      event: tournament.event,
+      link: tournament.link + '12',
+      message: 'Updated message',
+      percentage: '0.50'
+    }
+
+    await db.updateTournament(update);
+
+    const post = await db.getTournamentByNameAndEvent(tournament.name, event.name);
+
+    assert.deepEqual(post, update);
+  });
+
+  it('should get all events', async () => {
+    const events = [];
+
+    for (let i = 0; i < 5; i++) {
+      const event = {
+        name: `${i}a`,
+        start: 1586372294397,
+        end: 1586372196415,
+        ircUri: 'irc://irc.freenode.net:6667/#truth',
+        open: false
+      };
+
+      await db.createEvent(event);
+      events.push(event);
+    }
+
+    const indexed = await db.getEvents();
+    assert.equal(indexed.length, events.length);
+  });
+
+  it('should create/get payments', async () => {
+    const event = {
+      name: 'get-payments',
+      start: 1586372294397,
+      end: 1586372196415,
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
+    };
+
+    await db.createEvent(event);
+
+    const user = {
+      nick: 'create',
+      server: 'irc.freenode.net',
+      isSponsor: false
+    };
+
+    await db.createUser(user);
+
+    await db.addUserToEventByNickAndServer({
+      nick: user.nick,
+      server: user.server,
+      event: event.name
+    });
+
+    const payment = {
+      nick: user.nick,
+      server: user.server,
+      event: event.name,
+      txid: '00'.repeat(32),
+      type: 'btc',
+      outputIndex: 0
+    };
+
+    await db.createPayment(payment);
+
+    const result = await db.getPaymentsByNickAndServerAndEvent(user.nick, user.server, event.name);
+
+    assert.equal(result.length, 1);
+    assert.equal(result[0].type, payment.type);
+    assert.equal(result[0].txid, payment.txid);
+    assert.equal(result[0].outputIndex, payment.outputIndex);
+  });
+
+  it('should get address user pairs for event', async () => {
+    const event = {
+      name: `address-user-pairs`,
+      start: 1586372294397,
+      end: 1586372196415,
+      link: 'https://winner.ogh',
+      message: 'open your eyes',
+      ircUri: 'irc://irc.freenode.net:6667/#truth',
+      open: false
+    };
+
+    await db.createEvent(event);
 
     const users = [];
+    const addrs = [];
+
     for (let i = 0; i < 2; i++) {
       const user = {
-        nick: `user-${str}-${i}`,
+        nick: `user-${i}`,
         server: 'irc.freenode.net',
         isSponsor: false,
-        link: `twitter.com/user-${str}-${i}`
+        link: `twitter.com/user-${i}`
       };
 
       await db.createUser(user);
+      await db.addUserToEventByNickAndServer({
+        nick: user.nick,
+        server: user.server,
+        event: event.name
+      });
       users.push(user);
     }
 
     for (let [i, user] of users.entries()) {
       const addresses = [
-        `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}-${i}`,
-        `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}-${i}`,
-        `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${str}-${i}`
+        `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${randomStr(3)}`,
+        `hs1q3hgrv7em307swjp5h2cdlhk3x050ah6sxje5uu${randomStr(3)}`
       ];
 
-      // add addresses
-      for (let i = 0; i < 3; i++) {
-        const address = {
-          nick: user.nick,
-          server: user.server,
-          event: `address-user-pairs-${str}`,
-          address: addresses[i]
-        };
+      const address = {
+        nick: user.nick,
+        server: user.server,
+        event: event.name,
+        address: addresses[i]
+      };
 
-        await db.createAddress(address);
-      }
+      await db.createAddress(address);
+      addrs.push(addresses[i]);
     }
 
-    // TODO: fix this
     const result = await db.getUserAddressesByEvent(event.name);
+    assert.equal(result.length, 2);
 
-    console.log(result);
+    const [pair1, pair2] = result;
+
+    assert.equal(pair1.user.nick, users[0].nick);
+    assert.equal(pair2.user.nick, users[1].nick);
+
+    assert.equal(pair1.address.address, addrs[0])
+    assert.equal(pair2.address.address, addrs[1])
   });
 
   it('should get proven addresses', async () => {
     this.skip();
-
   });
 });
+
+function randomStr(len) {
+  assert(len <= 36);
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, len);
+}
